bd9267daa4b8cf478a60b92736f3920ae17586b5
Garrick Sherman <gsherma2@illinois.edu>
2016-10-26 13:42:27 -0700

    allow limits on number of lines read

--- src/main/java/edu/gslis/utils/readers/RelevanceModelReader.java
+++ src/main/java/edu/gslis/utils/readers/RelevanceModelReader.java
@@ -1,9 +1,12 @@
 package edu.gslis.utils.readers;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Scanner;
 
 import edu.gslis.textrepresentation.FeatureVector;
 
@@ -12,16 +15,56 @@ public class RelevanceModelReader extends Reader {
 	public static final String TERM_FIELD = "TERM";
 	public static final String SCORE_FIELD = "SCORE";
 	
+	private static final int DEFAULT_TERM_COUNT = 20;
+	
 	private FeatureVector vector;
 	
 	public RelevanceModelReader(File file) {
+		this(file, DEFAULT_TERM_COUNT);
+	}
+	
+	public RelevanceModelReader(File file, int termCount) {
 		super(Arrays.asList(SCORE_FIELD, TERM_FIELD));
-		read(file);
+		read(file, termCount);
 		createFeatureVector();
 	}
 	
 	public FeatureVector getFeatureVector() {
 		return vector;
+	}
+	
+	public void read(File file, int count) {
+		try {
+			int l = 0;
+			Scanner scanner = new Scanner(file);
+			while (scanner.hasNextLine()) {
+				l++;
+				if (l > count) {
+					break;
+				}
+
+				String line = scanner.nextLine();
+				
+				Map<String, String> field = new HashMap<String, String>();
+
+				String[] parts = line.split(delimiter);
+				for (int i = 0; i < parts.length; i++) {
+					String fieldValue = parts[i].trim();
+					String fieldName;
+					try {
+						fieldName = fields.get(i);
+					} catch (IndexOutOfBoundsException e) {
+						fieldName = "Field"+i;
+					}
+					field.put(fieldName, fieldValue);
+				}
+				
+				results.add(field);
+			}
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			System.err.println("File not found: "+file.getAbsolutePath());
+		}
 	}
 	
 	private void createFeatureVector() {
